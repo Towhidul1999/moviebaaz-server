6d5270c87535609e2103dea86d01fea21cee65fd
Towhidul1999 <towhidulsislam@gmail.com>
2021-09-26 14:22:58 +0600

    login with jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 
 // REGISTER
@@ -18,5 +19,29 @@ try{
     res.status(500).json(err)
 }
 });
+
+// LOGIN
+router.post("/login", async (req, res) => {
+    try{
+        const user = await User.findOne({email: req.body.email});
+        !user && res.status(401).json("Wrong Email Or Password");
+
+        const bytes  = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+        originalPassword !== req.body.password &&
+        res.status(401).json("Wrong Email Or Password");
+
+        const accessToken = jwt.sign({id: user._id, isAdmin: user.isAdmin},
+            process.env.SECRET_KEY,
+            {expiresIn: "5d"});
+
+        const {password, ...info} = user._doc;
+
+        res.status(200).json({...info, accessToken});
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router;
